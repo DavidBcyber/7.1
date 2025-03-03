90d55d47b67d55e4f01dfc60c7c5d78ebbfc7bad
DavidBcyber <dbustos28@amarillocollege.com>
2025-03-02 21:43:24 -0600

    Update Program.cs

--- 7.1/Program.cs
+++ 7.1/Program.cs
@@ -13,7 +13,7 @@ namespace _7._1
             public string Title { get; set; }
             public string Author { get; set; }
             public string Genre { get; set; }
-            public string Price { get; set; }
+            public int Price { get; set; }
         }
         static void Main(string[] args)
         {
