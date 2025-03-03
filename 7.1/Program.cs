1ea33496223a07f84eb149724ee78f672c7bbd19
xbatm <xbatm@MSI>
2025-03-02 20:25:08 -0600

    Add project files.

---
+++ 7.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1
+{
+    internal class Program
+    {
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public string Price { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            var nameTitle = title.Select(title => title);
+            Console.WriteLine(nameTitle);
+
+            var nameAuthor = author.Select(author => author);
+
+            var sortedrPrice = prices.OrderBy(prices => prices);
+            Console.WriteLine(sortedrPrice);
+
+            var groubByGenre = genres.GroupBy(genres => genres);
+            Console.WriteLine(groubByGenre);
+        }
+    }
+}
